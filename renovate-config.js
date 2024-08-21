0dded5bfbf6d40388401a8ae85dccac96e94fb75
Paul Relf <paulrelf@gmail.com>
2024-08-21 16:49:59 +1000

    test

---
+++ renovate-config.js
@@ -0,0 +1,12 @@
+  enabledManagers: [
+    "custom.regex"
+]
+  customManagers: [
+    {
+      customType: "regex",
+      fileMatch: ["^Dockerfile$"],
+      matchStrings: ["ARG FRREXPORTER_VERSION=v(?<currentValue>.*?)"],
+      depNameTemplate: "github.com/tynany/frr_exporter",
+      datasourceTemplate: "go"
+    }
+  ]
