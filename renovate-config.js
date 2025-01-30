f0f4cabd32b0504923c28669a8a9ef49ecba724c
MisterB1 <94657669+MisterB1@users.noreply.github.com>
2025-01-30 16:14:30 +1100

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,13 +1,4 @@
 module.exports = {
 "platform": "github",
 "autodiscover": true,
-"customManagers": [
-    {
-      "customType": "regex",
-      "fileMatch": ["^Dockerfile$"],
-      "matchStrings": [
-          "# renovate: datasource=(?<datasource>[a-z-]+?)(?: depName=(?<depName>.+?))\\s(?:ARG|ENV) .+?_VERSION=(?<currentValue>.+?)\\s"
-	]
-    }
-  ]
 }
